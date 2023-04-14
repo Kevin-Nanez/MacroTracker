01b778236646bc6fe4a67425bcf3d07a01e474ca
kevin Nañez <kevinn2304@gmail.com>
2023-04-14 14:09:23 -0600

    importé jquery, modificaciones en paginas y primera funcion js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    // redirigir a diario cuando se de click en un boton
+    $(".redirigirDiario").on("click", function () {
+        console.log("clisk")
+        window.location.href= "../Pages/diario.html";
+    });
+
+
+
+});
