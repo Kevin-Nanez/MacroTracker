0287a9aeb9ecb02c8b7934b3d93b7512fda0ed62
kevin Nañez <kevinn2304@gmail.com>
2023-04-15 19:33:22 -0600

    antes de iniciar el modal

---
+++ js/diario.js
@@ -0,0 +1,16 @@
+/*
+$(document).ready(function () {
+
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        language: 'es'
+    });
+
+    // Cierra el modal al seleccionar una fecha
+    $('.datepicker').on('changeDate', function () {
+        $('#calendarioModal').modal('hide');
+    });
+
+
+});
+*/
--- js/script.js
+++ js/script.js
@@ -1,68 +1,77 @@
 
 $(document).ready(function () {
-    // redirigir a diario cuando se de click en un boton
-    $(".redirigirDiario").on("click", function () {
-        event.preventDefault();
-        window.location.href= "../Pages/diario.html";
-    });
-    // redirigir a iniciar sesion cuando se de click en un boton
-    $(".redirigirLogin").on("click", function () {
-        event.preventDefault();
-        window.location.href= "../Pages/login.html";
-    });
-    $(".redirigirAgregar").on("click", function () {
-        event.preventDefault();
-        window.location.href= "../Pages/agregaraAlimento.html";
-    });
+  // redirigir a diario cuando se de click en un boton
+  $(".redirigirDiario").on("click", function () {
+    event.preventDefault();
+    window.location.href = "../Pages/diario.html";
+  });
+  // redirigir a iniciar sesion cuando se de click en un boton
+  $(".redirigirLogin").on("click", function () {
+    event.preventDefault();
+    window.location.href = "../Pages/login.html";
+  });
+  $(".redirigirAgregar").on("click", function () {
+    event.preventDefault();
+    window.location.href = "../Pages/agregaraAlimento.html";
+  });
 
-    $(".redirigirCrearar").on("click", function () {
-        event.preventDefault();
+  $(".redirigirCrearar").on("click", function () {
+    event.preventDefault();
 
-        window.location.href= "../Pages/crearAlimento.html";
-    });
+    window.location.href = "../Pages/crearAlimento.html";
+  });
 
-    $(".info_alimento").on("click", function () {
-      event.preventDefault();
+  $(".redirigirNoticias").on("click", function () {
+    event.preventDefault();
 
-      window.location.href= "../Pages/infoalimento.html";
+    window.location.href = "../Pages/noticias.html";
+  });
+
+  $(".info_alimento").on("click", function () {
+    event.preventDefault();
+
+    window.location.href = "../Pages/infoalimento.html";
   });
 
 
-    
 
-    //enviar info crear alimento
-    $('form').submit(function(event) {
-        event.preventDefault(); // Evita que se envíe el formulario automáticamente
-    
-        // Verifica que todos los campos tengan un valor
-        var camposVacios = false;
-        $(this).find('input[type!="submit"],textarea').each(function() {
-          if ($(this).val() === '') {
-            camposVacios = true;
-            return false; // Sale del bucle si encuentra un campo vacío
-          }
-        });
-    
-        // Si hay algún campo vacío, muestra un mensaje de error
-        if (camposVacios) {
-          alert('Por favor, completa todos los campos antes de enviar el formulario');
-          return;
-        }else{
-            setTimeout(()=>{
-                window.location.href= "../Pages/crearAlimento.html";
-            }
-            ,1000)
-        }
 
-        
-        // Si llegamos hasta aquí, significa que todos los campos tienen un valor
-        // Puedes enviar el formulario usando AJAX, por ejemplo:
-       // var formData = $(this).serialize();
-        //$.post($(this).attr('action'), formData, function(response) {
-          // Procesa la respuesta del servidor aquí
-        //});
-      });
+  //enviar info crear alimento
+  $('#formularioCrearAlimento').submit(function (event) {
+    event.preventDefault(); // Evita que se envíe el formulario automáticamente
 
-    
+    // Verifica que todos los campos tengan un valor
+    var camposVacios = false;
+    $(this).find('input[type!="submit"],textarea').each(function () {
+      if ($(this).val() === '') {
+        camposVacios = true;
+        return false; // Sale del bucle si encuentra un campo vacío
+      }
+    });
+
+    // Si hay algún campo vacío, muestra un mensaje de error
+    if (camposVacios) {
+      alert('Por favor, completa todos los campos antes de enviar el formulario');
+      return;
+    } else {
+      setTimeout(() => {
+        window.location.href = "../Pages/crearAlimento.html";
+      }
+        , 1000)
+    }
+
+
+    // Si llegamos hasta aquí, significa que todos los campos tienen un valor
+    // Puedes enviar el formulario usando AJAX, por ejemplo:
+    // var formData = $(this).serialize();
+    //$.post($(this).attr('action'), formData, function(response) {
+    // Procesa la respuesta del servidor aquí
+    //});
+  });
+
+
+
+  
+
 
 });
